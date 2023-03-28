d97a68f31b167f4fe8f990295502f02c3345531d
makaempffer <ma.kaempffer@duocuc.cl>
2023-03-28 14:57:28 -0300

    Added entity movement

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+
+
+//! Shows how to render simple primitive shapes with a single color.
+
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_startup_system(spawn_entities)
+        .add_system(move_circle)
+        .run();
+}
+#[derive(Component)]
+struct Entity;
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    // Circle
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+        material: materials.add(ColorMaterial::from(Color::PURPLE)),
+        transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
+        ..default()
+    }).insert(Entity);
+
+}
+
+fn move_circle(time: Res<Time>, mut query: Query<&mut Transform, With<Entity>>) {
+    for mut transform in query.iter_mut() {
+        transform.translation.x += 10.0 * time.delta_seconds();
+    }
+}
+
+fn spawn_entities(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
+    let amount: i32 = 10;
+    for _i in 0..amount {
+        commands.spawn(MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+            material: materials.add(ColorMaterial::from(Color::PURPLE)),
+            transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
+            ..default()
+        }).insert(Entity);
+    }
+}
